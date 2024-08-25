e00d3da8d3bca3dc7585d290d1df856e6c1aeddd
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-08-25 14:31:34 +0400

    Initial Commit

---
+++ ToDoList (Draft)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList (Draft)
+//
+//  Created by Mac Alexander on 25.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
